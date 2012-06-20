e9cae300e34e1d79550ec50d332a954877cad4f4
Thorsten Fischer <thorsten@froschi.org>
2012-06-21 01:02:11 +0200

    Added dependencies for lucid, precise.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,17 @@ maintainer       "Thorsten Fischer"
 maintainer_email "thorsten@froschi.org"
 license          "Apache 2.0"
 description      "Installs/Configures bind9-host"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 %w/ubuntu/.each { |os| supports os }
+
+depends          "libbind9", ">= 0.0.1"
+depends          "libcap", ">= 0.0.1"
+depends          "libdns", ">= 0.0.1"
+depends          "libgssapi-krb5", ">= 0.0.1"
+depends          "libisc", ">= 0.0.1"
+depends          "libisccfg", ">= 0.0.1"
+depends          "liblwres", ">= 0.0.1"
+depends          "libssl", ">= 0.0.1"
+depends          "libxml2", ">= 0.0.1"
